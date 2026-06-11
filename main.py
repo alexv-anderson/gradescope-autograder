a908cf27cfbccc39186bf81fa8517846b3261163
alexv-anderson <opiemike613@yahoo.com>
2026-06-11 12:40:00 -0500

    Handle input at end of a line

--- main.py
+++ main.py
@@ -3,6 +3,56 @@ import json
 import re
 
 
+class Criterion:
+    def __init__(self, d: dict):
+        if "lines" not in d:
+            d["lines"] = 1
+        self._datum = d
+
+        self.endsWithInput = "endsWithInput" in d and d["endsWithInput"]
+    
+    def __str__(self) -> str:
+        return json.dumps(self._datum)
+    
+    def apply(self, num: int, l: str) -> Tuple:
+        msg = None
+
+        req = self._datum["requirement"]
+        if req == "ignore":
+            # No requirements for this line
+            pass
+        elif req == "exact":
+            exp = self._datum["expected"]
+            
+            match_prefix_with_input = self.endsWithInput and l.startswith(exp)
+            full_match = not self.endsWithInput and l == exp
+
+            if not (full_match or match_prefix_with_input):
+                msg = f"At line {num} expected '{exp}' but got '{l}'"
+
+        elif req == "pattern":
+            if not re.match(self._datum["expected"], l):
+                msg = f"'{l}' on line {num} has an incorrect pattern"
+        else:
+            msg = f"ERROR: Unrecognized requirement for line {num}"
+        
+        return (msg is None, msg)
+    
+    def consume(self, l: str) -> str:
+        pattern = self._datum["expected"]
+        if re.match(pattern, l):
+            remain = l[re.search(pattern, l).span()[1]:]
+            if len(remain) > 0:
+                return remain
+        return None
+
+    def update(self):
+        self._datum["lines"] -= 1
+    
+    def is_exhausted(self):
+        return self._datum["lines"] < 1
+    
+
 class Rubric:
     def __init__(self, fp: str):
         with open(fp, "r") as f:
@@ -11,31 +61,43 @@ class Rubric:
         self._output = data["output"]
     
     def grade(self, out_fp: str) -> dict:
-        line_feedback = []
+        print("\n----------------------\n\n")
 
         criterion = self._load_next_criterion()
 
         with open(out_fp, "r") as f:
-            for i, l in enumerate(f):
-                if criterion["lines"] < 1:
-                    criterion = self._load_next_criterion()
-                
-                print(f"Line {i+1}: '{l}' -> {json.dumps(criterion)}")
+            line_num = 1
+            for l in f:
+                while True:
+                    if criterion is None:
+                        return (False, f"No criterion for '{l}' on line {line_num}")
 
-                req = criterion["requirement"]
-                if req == "ignore":
-                    # No requirements for this line
-                    pass
-                elif req == "exact":
-                    exp = criterion["expected"]
-                    if l != exp:
-                        return (False, f"At line {i+1} expected '{exp}' but got '{l}'")
-                elif req == "pattern":
-                    if not re.match(criterion["expected"], l):
-                        return (False, f"'{l}' on line {i+1} has an incorrect pattern")
-                
-                criterion["lines"] -= 1
-        
+                    print(f"Line {line_num}: '{l}' -> {criterion}")
+
+                    result = criterion.apply(line_num, l)
+                    print(f"\tResult: {result}")
+                    if not result[0]:
+                        return result
+                    
+                    line_num += 1
+                    
+                    criterion.update()
+            
+                    if criterion.endsWithInput:
+                        l = criterion.consume(l)
+                        print(l)
+                    
+                    if criterion.is_exhausted():
+                        print(f"\tExhausted criterion: {criterion}")
+                        criterion = self._load_next_criterion()
+                        print(f"\tLoaded: {criterion}")
+
+                    get_next_line = criterion is not None and not criterion.endsWithInput
+                    end_of_rubric = criterion is None
+
+                    if get_next_line or end_of_rubric:
+                        break
+
         return (True, None)
     
     def _load_next_criterion(self):
@@ -43,10 +105,8 @@ class Rubric:
             return None
 
         req = self._output.pop(0)
-        if "lines" not in req:
-            req["lines"] = 1
         
-        return req
+        return Criterion(req)
 
 
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -1,7 +1,121 @@
 
 import unittest
 
-from main import Rubric
+from main import Criterion, Rubric
+
+
+class CriterionTest(unittest.TestCase):
+    def test_lines_default(self):
+        criterion = Criterion({})
+
+        self.assertIn("lines", criterion._datum)
+        self.assertEqual(1, criterion._datum["lines"])
+
+        criterion = Criterion({"lines": 2})
+        self.assertEqual(2, criterion._datum["lines"])
+
+    def test_ends_with_input_default(self):
+        criterion = Criterion({})
+        self.assertFalse(criterion.endsWithInput)
+
+        criterion = Criterion({"endsWithInput": False})
+        self.assertFalse(criterion.endsWithInput)
+
+        criterion = Criterion({"endsWithInput": True})
+        self.assertTrue(criterion.endsWithInput)
+
+    def test_apply_exact_with_no_input(self):
+        criterion = Criterion({
+            "requirement": "exact",
+            "expected": "What is your favorite number?"
+        })
+
+        res = criterion.apply(5, "What is your favorite number?")
+        self.assertTrue(res[0])
+        self.assertIsNone(res[1])
+
+        res = criterion.apply(5, "What is your favorite number")
+        self.assertFalse(res[0])
+        self.assertTrue(res[1].startswith("At line 5"))
+
+    def test_apply_exact_with_input(self):
+        criterion = Criterion({
+            "requirement": "exact",
+            "expected": "What is your favorite number?",
+            "endsWithInput": True
+        })
+
+        res = criterion.apply(5, "What is your favorite number?Twice your favorite number is 32")
+        self.assertEqual((True, None), res)
+
+        res = criterion.apply(5, "What is your favorite numberTwice your favorite number is 32")
+        self.assertFalse(res[0])
+        self.assertTrue(res[1].startswith("At line 5"))
+    
+    def test_apply_pattern_with_no_input(self):
+        criterion = Criterion({
+            "requirement": "pattern",
+            "expected": "Welcome to [A-Za-z\\']+ maze!"
+        })
+
+        res = criterion.apply(5, "Welcome to Alex's maze!")
+        self.assertEqual((True, None), res)
+
+        res = criterion.apply(5, "Welcome to Alex's maze")
+        self.assertFalse(res[0])
+        self.assertTrue("on line 5" in res[1])
+    
+    def test_apply_pattern_with_input(self):
+        criterion = Criterion({
+            "requirement": "pattern",
+            "expected": "Welcome to [A-Za-z\\']+ maze!",
+            "endsWithInput": True
+        })
+
+        res = criterion.apply(5, "Welcome to Alex's maze!Hello world")
+        self.assertEqual((True, None), res)
+
+        res = criterion.apply(5, "Welcome to Alex's mazeHello world")
+        self.assertFalse(res[0])
+        self.assertTrue("on line 5" in res[1])
+
+    def test_consume_exact(self):
+        criterion = Criterion({
+            "requirement": "exact",
+            "expected": "Hello, world"
+        })
+
+        remain = criterion.consume("Hello, world!")
+        self.assertEqual("!", remain)
+
+        remain = criterion.consume("Hello, world")
+        self.assertIsNone(remain)
+
+    def test_consume_pattern(self):
+        criterion = Criterion({
+            "requirement": "pattern",
+            "expected": "Hello, world"
+        })
+
+        remain = criterion.consume("Hello, world!")
+        self.assertEqual("!", remain)
+
+        remain = criterion.consume("Hello, world")
+        self.assertIsNone(remain)
+
+    def test_update_exhausted(self):
+        criterion = Criterion({"lines": 2})
+
+        self.assertFalse(criterion.is_exhausted())
+
+        criterion.update()
+        self.assertFalse(criterion.is_exhausted())
+
+        criterion.update()
+        self.assertTrue(criterion.is_exhausted())
+
+        criterion.update()
+        self.assertTrue(criterion.is_exhausted())
 
 
 def get_rubric_results(file_name: str) -> Tuple:
@@ -9,10 +123,15 @@ def get_rubric_results(file_name: str) -> Tuple:
     return rubric.grade(f"test_res/{file_name}.txt")
 
 
-
-class MyTestCase(unittest.TestCase):
-    def test_separate_lines(self):
+class RubricTest(unittest.TestCase):
+    def test_separate_lines_with_no_input(self):
         results = get_rubric_results("seperate_lines_a")
+
+        self.assertTrue(results[0], results[1])
+        self.assertIsNone(results[1], "Passing tests should not have warning messages")
+
+    def test_separate_lines_with_input(self):
+        results = get_rubric_results("seperate_lines_b")
 
         self.assertTrue(results[0], results[1])
         self.assertIsNone(results[1], "Passing tests should not have warning messages")
